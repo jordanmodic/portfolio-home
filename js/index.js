107c39e92f4e4111c064a9affc220a751b44422a
jordanmodic <118031821+jordanmodic@users.noreply.github.com>
2023-05-08 10:35:16 -0700

    Added Files
    Added initial files for portfolio homepage

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+console.log("Your index.js file is loaded correctly!");
+$(".one").mouseover(function(){
+    $(".button-one").fadeIn();
+});
+$(".one").mouseleave(function(){
+    $(".button-one").fadeOut();
+})
+$(".two").mouseover(function(){
+    $(".button-two").fadeIn();
+});
+$(".two").mouseleave(function(){
+    $(".button-two").fadeOut();
+})
+$(".three").mouseover(function(){
+    $(".button-three").fadeIn();
+});
+$(".three").mouseleave(function(){
+    $(".button-three").fadeOut();
+})
